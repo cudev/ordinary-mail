c3ca0515eebe5235998e64eb8b61e9295ef76555
Andrii Smirnov <daysinditch@gmail.com>
2016-05-24 11:36:27 +0300

    PHPdoc added

--- src/CredentialsInterface.php
+++ src/CredentialsInterface.php
@@ -2,6 +2,11 @@
 
 namespace Cudev\OrdinaryMail;
 
+/**
+ * Just marker interface to find something with name and address
+ * Interface CredentialsInterface
+ * @package Cudev\OrdinaryMail
+ */
 interface CredentialsInterface
 {
     /** @return string */
